de4469a4c30be8f1105a684f6fa7b0b0ca241caa
Krister Looga <looga.krister@gmail.com>
2020-11-09 12:55:52 +0200

    updated merge script

--- merge.sh
+++ merge.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+array=("Training" "Test")
 index=0
-for f in data/fruits-360/Training/*
+for i in "${array[@]}"
 do
- fruitFolder=$(echo $f | rev | cut -d'/' -f 1 | rev )
- fruitFolderName=$(echo $fruitFolder | cut -d' ' -f1)
- mkdir -p mergedData/"$fruitFolderName"
- rsync -a --backup --suffix="$index" "$f"/ mergedData/"$fruitFolderName"
- index=$((index+1))
+	for f in data/fruits-360/"$i"/*
+	do
+	 fruitFolder=$(echo $f | rev | cut -d'/' -f 1 | rev )
+	 fruitFolderName=$(echo $fruitFolder | cut -d' ' -f1)
+	 mkdir -p mergedData/"$i"/"$fruitFolderName"
+	 rsync -a --backup --suffix="$index" "$f"/ mergedData/"$i"/"$fruitFolderName"
+	 index=$((index+1))
+	done
 done
